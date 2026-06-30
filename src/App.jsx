ad4f5ae9c35999c356521224745883138fc054a8
Hugo <hugomdmg@profesorestrazos.net>
2026-06-30 17:32:44 +0200

    add library react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import { Login } from "./pages/LoginPage/LoginPage";
 import { Register } from './pages/RegisterPage/RegisterPage'
